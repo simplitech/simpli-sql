80d3576ad00eb7130502ae35acbe9f9d4807587b
Joao Lippi <joao.lippi@simpli.com.br>
2019-12-04 22:35:30 -0300

    Changed DaoTest data source to be dynamic, allowing unit tests to be ran on any database

--- src/main/kotlin/br/com/simpli/sql/DaoTest.kt
+++ src/main/kotlin/br/com/simpli/sql/DaoTest.kt
@@ -1,10 +1,13 @@
 package br.com.simpli.sql
 
+import br.com.simpli.tools.ResourceLoader
 import com.mysql.jdbc.jdbc2.optional.MysqlConnectionPoolDataSource
 import org.junit.After
 import org.junit.AfterClass
+import java.security.InvalidParameterException
 import java.sql.Connection
 import java.sql.SQLException
+import java.util.*
 import java.util.logging.Level
 import java.util.logging.Logger
 import javax.naming.Context
@@ -12,9 +15,36 @@ import javax.naming.NamingException
 
 /**
  *
- * @author ricardomeira
+ * @author JoaoLippi
  */
-open class DaoTest(dsName: String, databaseName: String) {
+open class DaoTest private constructor(
+    user: String,
+    password: String ,
+    host: String,
+    port: Int,
+    dsName: String?,
+    databaseName: String?,
+    configPath: String?
+) {
+
+    constructor(
+        user: String,
+        password: String,
+        host: String,
+        port: Int,
+        dsName: String,
+        databaseName: String
+    ) : this (user, password, host, port, dsName, databaseName, null)
+
+    constructor(
+        dsName: String?,
+        databaseName: String?
+    ) : this ("root", "root", "localhost", 3306, dsName, databaseName, null)
+
+    @JvmOverloads
+    constructor(
+        configPath: String? = "/test.ds.properties"
+    ) : this ("root", "root", "localhost", 3306, null, null, configPath)
 
     fun getConnection(): Connection {
         return currentConnection!!
@@ -22,23 +52,33 @@ open class DaoTest(dsName: String, databaseName: String) {
 
     init {
         try {
+            val props = configPath?.run { ResourceLoader.getProperties(this) } ?: Properties()
+
             // sets up the InitialContextFactoryForTest as default factory.
             System.setProperty(Context.INITIAL_CONTEXT_FACTORY,
                     InitialContextFactoryForTests::class.java.name)
 
-            val dataSource = MysqlConnectionPoolDataSource()
-            dataSource.user = "root"
-            dataSource.setPassword("root")
-            dataSource.serverName = "localhost"
-            dataSource.port = 3306
-            dataSource.databaseName = databaseName
+            currentConnection = MysqlConnectionPoolDataSource().apply {
+                this.user = props["user"] as? String ?: user
+                this.setPassword(props["password"] as? String ?: password)
+                this.serverName = props["host"] as? String ?: host
+                this.port = props["port"] as? Int ?: port
+                this.databaseName = props["database"] as? String
+                    ?: databaseName
+                    ?: throw InvalidParameterException("Missing database name")
 
-            InitialContextFactoryForTests.bind(dsName, dataSource)
-            val ctx = InitialContextFactoryForTests().getInitialContext()
-            InitialContextFactoryForTests.bind("java:/comp/env", ctx!!)
-
-            currentConnection = dataSource.connection
-            currentConnection?.autoCommit = false
+                InitialContextFactoryForTests.bind(
+                    props["datasource"] as? String
+                    ?: dsName
+                    ?: throw InvalidParameterException("Missing data source name"),
+                    this
+                )
+                InitialContextFactoryForTests().getInitialContext()?.apply {
+                    InitialContextFactoryForTests.bind("java:/comp/env", this)
+                }
+            }.connection?.apply {
+                autoCommit = false
+            }
 
         } catch (ex: NamingException) {
             Logger.getLogger(DaoTest::class.java.name).log(Level.SEVERE, null, ex)
